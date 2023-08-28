732d2b8b32657afc481ca2ec96ad8c8e213a179f
MingSheng <102264706+mingsheng13@users.noreply.github.com>
2023-08-28 23:30:28 +0100

    Implemented ip component

---
+++ ip_input.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/util/ref.hpp"
+
+namespace ftxui
+{
+    Component IpInput(StringRef content);
+}
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <string>
+
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+
+#include "ip_input.h"
+using namespace ftxui;
+
+//example
+int main()
+{
+    std::string ipv4;
+    std::string mask;
+
+    auto ipInput = IpInput(&ipv4);
+    auto maskInput = IpInput(&mask);
+
+    auto container = Container::Vertical({ipInput, maskInput});
+
+    auto renderer = Renderer(container, [&]
+    {
+       return vbox(
+               {
+                   text("Enter ip:    "), ipInput->Render(),
+                   text("Enter mask:  "), maskInput->Render(),
+                   window(text("output"), vbox(
+                           {
+                               text(ipv4),
+                               text(mask)
+                           }))
+               });
+    });
+    auto screen = ScreenInteractive::TerminalOutput();
+    screen.Loop(renderer);
+}
