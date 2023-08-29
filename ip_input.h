3cd8dab564557583b4bdbf0f2d734db834ea9a11
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-08-29 22:00:26 +0200

    Apply clang-format

--- ip_input.h
+++ ip_input.h
@@ -3,7 +3,6 @@
 #include "ftxui/component/component_base.hpp"
 #include "ftxui/util/ref.hpp"
 
-namespace ftxui
-{
-    Component IpInput(StringRef content);
+namespace ftxui {
+Component IpInput(StringRef content);
 }
--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 #include <string>
 
-#include "ftxui/dom/elements.hpp"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
 
 #include "ip_input.h"
 using namespace ftxui;
 
-//example
-int main()
-{
-    std::string ipv4;
-    std::string mask;
+// example
+int main() {
+  std::string ipv4;
+  std::string mask;
 
-    auto ipInput = IpInput(&ipv4);
-    auto maskInput = IpInput(&mask);
+  auto ipInput = IpInput(&ipv4);
+  auto maskInput = IpInput(&mask);
 
-    auto container = Container::Vertical({ipInput, maskInput});
+  auto container = Container::Vertical({
+      ipInput,
+      maskInput,
+  });
 
-    auto renderer = Renderer(container, [&]
-    {
-       return vbox(
-               {
-                   text("Enter ip:    "), ipInput->Render(),
-                   text("Enter mask:  "), maskInput->Render(),
-                   window(text("output"), vbox(
-                           {
-                               text(ipv4),
-                               text(mask)
-                           }))
-               });
+  auto renderer = Renderer(container, [&] {
+    return vbox({
+        text("Enter ip:    "),
+        ipInput->Render(),
+        text("Enter mask:  "),
+        maskInput->Render(),
+        window(text("output"), vbox(
+                                   {
+                                       text(ipv4),
+                                       text(mask),
+                                   }, )),
     });
-    auto screen = ScreenInteractive::TerminalOutput();
-    screen.Loop(renderer);
+  });
+  auto screen = ScreenInteractive::TerminalOutput();
+  screen.Loop(renderer);
 }
